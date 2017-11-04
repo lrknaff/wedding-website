f620a3cad6fe82c7a2ef50a8bbc75d863a1e7840
lrknaff <lacey.knaff@gmail.com>
2017-11-04 14:54:19 -0600

    initial type styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
