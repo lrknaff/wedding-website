d7322c83bc6d80b5a075b1e46c9d8517ac5467b8
lrknaff <lacey.knaff@gmail.com>
2017-11-04 15:41:14 -0600

    initial header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,68 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
   render() {
     return (
       <header>
-        Im the Header
+        <a
+          className="header-home"
+          href="/"
+          activeStyle={{ color: '#FFFFFF;' }}
+        >
+          Lacey & Mike
+        </a>
+        <nav className="header-nav">
+          <ul className="header-nav-list">
+            <li>
+              <a
+                href="/"
+                activeStyle={{ color: '#FFFFFF;' }}
+              >
+                Our Story
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                activeStyle={{ color: '#FFFFFF;' }}
+              >
+                The Details
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                activeStyle={{ color: '#FFFFFF;' }}
+              >
+                Travel Tips
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                activeStyle={{ color: '#FFFFFF;' }}
+              >
+                Photos
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                activeStyle={{ color: '#FFFFFF;' }}
+              >
+                Registry
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <a
+          className="header-rsvp"
+          href="/"
+          activeStyle={{ color: '#FFFFFF;' }}
+        >
+          RSVP +
+        </a>
       </header>
     )
   }
